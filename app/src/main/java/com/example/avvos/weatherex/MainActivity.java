172adef2fb7181a932c24a354c494bf044123db9
Avvos <hmztkn@gmail.com>
2017-05-20 00:20:49 +0300

    Second Commit

--- app/src/main/java/com/example/avvos/weatherex/MainActivity.java
+++ app/src/main/java/com/example/avvos/weatherex/MainActivity.java
@@ -1,6 +1,15 @@
 package com.example.avvos.weatherex;
 
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.LayoutInflater;
@@ -15,10 +24,39 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TabLayout tabLayout1;
+    ViewPager viewPager1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager1 = (ViewPager) findViewById(R.id.viewPager1);
+        viewPager1.setAdapter(new CustomAdapter2(getSupportFragmentManager(), getApplicationContext()));
+
+        tabLayout1 = (TabLayout) findViewById(R.id.tabLayout1);
+        tabLayout1.setupWithViewPager(viewPager1);
+
+        tabLayout1.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
+
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager1.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                viewPager1.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                viewPager1.setCurrentItem(tab.getPosition());
+            }
+        });
+
 
         String[] foods = {"Salam", "Sucuk", "patates"};
 
@@ -39,5 +77,34 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
+    }
+
+    private class CustomAdapter2 extends FragmentPagerAdapter {
+        private String fragments [] = {"Fragment 1 ", "Fragment 2"};
+        public CustomAdapter2(FragmentManager supportFragmentManager, Context applicationContext) {
+            super(supportFragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new Fragment1();
+                case 1:
+                    return new Fragment2();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position){
+            return fragments[position];
+        }
     }
 }
